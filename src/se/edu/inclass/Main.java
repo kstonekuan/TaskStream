969f78370e8d3f0c2c94e44167b913e06b008f33
kstonekuan <kingstonkuan@u.nus.edu>
2020-09-18 17:16:58 +0800

    Change print statements to streams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -16,10 +16,17 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        System.out.println("Printing deadlines");
+//        printData(tasksData);
+//        System.out.println("Printing deadlines");
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+
+//        printDataUsingStreams(tasksData);
+
+        printDeadlinesUsingStreams(tasksData);
+
+        System.out.println("total number of deadlines using streams: " + countDeadlinesUsingStreams(tasksData));
 
     }
 
